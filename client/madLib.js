d703991cfcb9da936cf6f6c58628d018840b1979
Luke Schettenhelm <lukeschett@gmail.com>
2016-08-01 20:47:23 -0400

    finished mad lib

--- client/madLib.js
+++ client/madLib.js
@@ -2,9 +2,13 @@ Template.body.events({
 	'submit #madLibForm'(event) {
 		event.preventDefault();
 		let exclamation = event.target.exclamation.value;
-    let adverb = event.target.adverb.value;
-    let noun = event.target.noun.value;
-    let verb = event.target.verb.value;
-    console.log("'" + exclamation + "!' you say, as you " + adverb + " jump onto your " + noun + " and " + verb + " into the sunset.");
-  }
+    	let adverb = event.target.adverb.value;
+    	let noun = event.target.noun.value;
+    	let verb = event.target.verb.value;
+    	let madLib = "'" + exclamation + "!' you say, as you " + adverb + " jump onto your " + noun + " and " + verb + " into the sunset.";
+    	console.log(madLib);
+
+    	$("#createMadLib").hide();
+    	$("#createMadLib").text(madLib).fadeIn("slow").css("color", "#0000ff");
+  	}
 });
